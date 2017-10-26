4d05a3fc68897eb3549bb98c0713bf8b504d67ad
Salvatore Capuozzo <sasy.19.05.96@gmail.com>
2017-10-26 17:31:17 +0200

    Implementazione algoritmo accuratezza e separati array degli scartati

--- main.cpp
+++ main.cpp
@@ -1,90 +1,88 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
-
-void programmma(char* argv);
-
-int main()
-{ 
-DIR *dirp = NULL;
-
-struct dirent *dp;
-
-dirp = opendir("dati\\");
-int k=0;
-while (dirp) 
-{
-
-
-if ((dp = readdir(dirp)) != NULL) 
-{
-k++;
-puts(dp->d_name);
-if(k>2)programmma(dp->d_name);
-} 
-else
-{
-closedir(dirp);
-dirp = NULL;
-}
-}
-return 0;
-}
-
-
-
-
-
-
-
 #include <iostream>
 #include <stdlib.h>
 #include <cstring>
 
 using namespace std;
 
-#include <stdio.h>
 #include <string.h>
 #define k 55
 #define N 10
 #include "funzioni.h"
 
+void programmma(char* argv);
+
+int main() {
+    DIR *dirp = NULL;
+
+    struct dirent *dp;
+
+    //dirp = opendir("dati\\");
+    dirp = opendir("/Users/salvatorecapuozzo/ThresholdForClassifiers/dati");
+    int p=0;
+    while (dirp) {
+        if ((dp = readdir(dirp)) != NULL) {
+            p++;
+            puts(dp->d_name);
+            if(p>2)
+                programmma(dp->d_name);
+        } else {
+            closedir(dirp);
+            dirp = NULL;
+        }
+    }
+    return 0;
+}
+
 void fine(analis* list);
 int u=900;
+
 void programmma(char* argv) {
+    n = 0;
 	int scartati[N+1];
 	int esatti[N+1];
 	int sbagliati[N+1];
+    
+    int scartatiEsatti[N+1];
+    int scartatiSbagliati[N+1];
 
 	for(int i=0;i<N+1;i++)
-	sbagliati[i]=esatti[i]=scartati[i]=0;
-	string nome_file;
+        sbagliati[i]=esatti[i]=scartati[i]=scartatiEsatti[i]=scartatiSbagliati[i]=0;
+        string nome_file;
 
 //	cout<<"programma di test\n";
 	
 	
-	nome_file=argv;
-		
-	nome_file="dati\\" + nome_file; 
-	cout<<"\nFile: "<<nome_file;
+        nome_file=argv;
+    
+        // Salvatore
+        nome_file = "/Users/salvatorecapuozzo/ThresholdForClassifiers/dati/" + nome_file;
+        // Filippo
+        //nome_file="dati\\" + nome_file;
+    
+        cout<<"\nFile: "<<nome_file;
 
-		
+        p=fopen(&nome_file[0],"r");
+        while(!p) {
+            
+        cout<<"\nInserisci nome file: ";
+        cin>>nome_file;
 
-p=fopen(&nome_file[0],"r");
-		while(!p)
-		{
-		
-	cout<<"\tINserisci nome file: ";
-	cin>>nome_file;
-
-	//nome_file=argv[1];
-		
-	nome_file="dati\\" + nome_file; 
-	cout<<"\nFile: "<<nome_file;
-//	nome_file="test.csv";
-	//cin>>nome_file;
-	p=fopen(&nome_file[0],"r");
-	if(!p)cout<<"Errore apertura file!";
+        //nome_file=argv[1];
+            
+        // Salvatore
+        nome_file = "/Users/salvatorecapuozzo/ThresholdForClassifiers/dati/" + nome_file;
+        // Filippo
+        //nome_file="dati\\" + nome_file;
+            
+        cout<<"\nFile: "<<nome_file;
+    //    nome_file="test.csv";
+        //cin>>nome_file;
+        p=fopen(&nome_file[0],"r");
+        if(!p)
+            cout<<"\nErrore apertura file!";
 	}
 
 			
@@ -95,10 +93,14 @@ p=fopen(&nome_file[0],"r");
 	
 	nome_file=argv;
 	
-
-nome_file="risultati\\Risuttati_" + nome_file; 
-//cout<<nome_file;
-			 FILE* csv;
+    // Salvatore
+    nome_file = "/Users/salvatorecapuozzo/ThresholdForClassifiers/risultati/Risultati_" + nome_file;
+    // Filippo
+    //nome_file="risultati\\Risuttati_" + nome_file;
+    
+    //cout<<nome_file;
+    
+    FILE* csv;
 	//csv=fopen(&nome_file[0],"w");
 	//csv=stdout;
 	csv=fopen(&nome_file[0],"w");
@@ -106,45 +108,48 @@ nome_file="risultati\\Risuttati_" + nome_file;
 //	analis list[150];
 	analis app;
 	
-	if(p)
-	{
+	if(p) {
 	
-f1();
-n=n-2;
-int ii=0;
-for(int i=0;i<u;i++){
-	if(p){
+    f1();
+    n=n-2;
+    int ii=0;
+    for(int i=0;i<u;i++){
+        if(p){
+            memset (app.attuale,0.0,n);
+            f1(app.attuale);
+            f1(&app);
+            f1(app.probabilita);
+            
+            if(!control(&app)){
+            cout<<"\terrore: "<<i<<endl;
+            u=i-1;
+            u--;
+            }
+            for (int j=0;j<N+1;j++){
+            if(app.p_max>=(1.0*j)/N){
+                if(app.corretto)
+                    esatti[j]++;
+                else
+                    sbagliati[j]++;
+            } else {
+                scartati[j]++;
+                if(app.corretto)
+                    scartatiEsatti[j]++;
+                else
+                    scartatiSbagliati[j]++;
+            }
+            }
+            
+            //cout<<"attuale: ";    cout <<list[i].attuale<<endl;
+            //cout<<"coretto: ";    cout <<list[i].corretto<<endl;
+            //cout<<"numeri: ";     //for(int j=0;j<n;j++)cout<<" "<<list[i].probabilita[j]<<endl;
 
-
-	
-		memset (app.attuale,0.0,n);
-		f1(app.attuale);
-		f1(&app);
-		f1(app.probabilita);
-		
-		if(!control(&app)){
-		cout<<"\terrore: "<<i<<endl;
-		u=i-1;
-		u--;
-		}
-		for(int j=0;j<N+1;j++){		
-		if(app.p_max>=(1.0*j)/N){
-			if(app.corretto)esatti[j]++;
-			else sbagliati[j]++;
-		}
-		else scartati[j]++;
-		}
-		
-		//cout<<"attuale: ";	cout <<list[i].attuale<<endl;
-		//cout<<"coretto: ";	cout <<list[i].corretto<<endl;
-		//cout<<"numeri: ";	 //for(int j=0;j<n;j++)cout<<" "<<list[i].probabilita[j]<<endl;
-
-	}
-	else{
-		u--;
-		cout<<"errori I= "<<i<<endl;
-	}
-}
+        }
+        else{
+            u--;
+            cout<<"errori I= "<<i<<endl;
+        }
+    }
 
 }
 	else cout<<"errore file non aperto!";
@@ -152,18 +157,19 @@ fclose(p);
 char s1;
 
 
-	cout<<"\ncoef;\t"<<"scartati"<<";\t"<<"esatti;"<<"\t"<<"\tsbagliati"<<";"<<"  \t  precisione;"<<endl;
-		fprintf(csv,"coef;scartati;esatti;sbagliati;precisione;\n");
+	cout<<"\ncoef;\t"<<"scartatiEsatti"<<";\t"<<"scartatiSbagliati"<<";\t"<<"esatti;"<<"\t"<<"\tsbagliati"<<";"<<"\taccuratezza"<<";"<<"\tprecisione;"<<endl;
+    fprintf(csv,"coef;scartatiEsatti;scartatiSbagliati;esatti;sbagliati;accuratezza;precisione;\n");
 	
 for(int j=0;j<N+1;j++){
 
 //cout<<"file generat con suecesso, premere un tasto per chiudere!";
+    float accuratezza = (esatti[j]+scartatiSbagliati[j])/(esatti[j]+sbagliati[j]+scartatiEsatti[j]+scartatiSbagliati[j]);
 	float precisione=esatti[j]/((esatti[j]+sbagliati[j])*1.0);
 		//cout<<"precisionerattezza= "<<precisione<<endl;
 		
 			char str[100];
-		printf("%.1f; \t%8d;  \t%6d;   \t %6d; \t\t%.3f;\n",j/(N*1.0),scartati[j],esatti[j],sbagliati[j],precisione);
-		sprintf(str,"%f;%d;%d;%d;%f;\n",j/(N*1.0),scartati[j],esatti[j],sbagliati[j],precisione);
+		printf("%.1f; \t%8d;  \t%6d;   \t %6d;  \t %6d;  \t\t%.3f; \t%.3f;\n",j/(N*1.0),scartatiEsatti[j],scartatiSbagliati[j],esatti[j],sbagliati[j],accuratezza,precisione);
+    sprintf(str,"%f;%d;%d;%d;%d;%f;%f;\n",j/(N*1.0),scartatiEsatti[j],scartatiSbagliati[j],esatti[j],sbagliati[j],accuratezza,precisione);
 		//cout<<i<<";"<<scartati<<";"<<esatti<<";"<<sbagliati<<";"<<precisione<<endl;
 	char * pch2;
 	char * pch3;
@@ -186,14 +192,13 @@ for(int j=0;j<N+1;j++){
 //	fine(list);
 
 }
-
+/*
 void fine(analis* list){
-		
-		string nome_file;
-			cout<<"inserisci il file su cui vuoi salvare i dati (nomefile.csv) : ";
-			cin>>nome_file;
-			nome_file="risultati\\" + nome_file; 
-			 FILE* csv;
+    string nome_file;
+    cout<<"inserisci il file su cui vuoi salvare i dati (nomefile.csv) : ";
+    cin>>nome_file;
+    nome_file="risultati\\" + nome_file;
+    FILE* csv;
 	//csv=fopen(&nome_file[0],"w");
 	//csv=stdout;
 	freopen(&nome_file[0],"w",stdout);
@@ -241,4 +246,4 @@ void fine(analis* list){
 	}
 	
 }
-
+*/
